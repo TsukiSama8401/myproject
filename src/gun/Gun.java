c488e6a7df069f8efc54d47a7da00e59c17b3650
sleep <sleep@192.168.0.16>
2017-03-14 00:07:11 +0800

    java homework20170309

---
+++ src/gun/Gun.java
@@ -0,0 +1,18 @@
+package gun;
+
+public class Gun {
+	String name;
+	String type;
+	int rds;
+	int range;
+	int leight;
+	public Gun(String name,String type,int rds,int range,int leight){
+		this.name = name;
+		this.type = type;
+		this.rds = rds;
+		this.range = range;
+		this.leight = leight;
+		
+	}
+
+}
---
+++ src/gun/GunTester1.java
@@ -0,0 +1,15 @@
+package gun;
+
+public class GunTester1 {
+
+	public static void main(String[] args) {
+		Gun sg1 = new Gun("KS-23", "shotgun", 3, 150, 1040);
+		System.out.println("Name:" + sg1.name);
+		System.out.println("Type:" + sg1.type);
+		System.out.println("rds:" + sg1.rds);
+		System.out.println("Range:" + sg1.range + "m");
+		System.out.println("Leight:" + sg1.leight + "mm");
+
+	}
+
+}
