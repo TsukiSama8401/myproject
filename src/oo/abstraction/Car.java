cac0d395a777e5fc624cb374a18c84455117b5b1
Yohanei <sleepcat10235269@gmail.com>
2017-03-14 15:57:37 +0800

    java20170314

--- src/oo/abstraction/Car.java
+++ src/oo/abstraction/Car.java
@@ -6,11 +6,11 @@ public class Car {
 		int cc;
 		String name;
 		String type;
-		public Car (String brand,int cc,String name,String type){
+		public Car (String brand,String name,int cc){
 			this.brand = brand;
 			this.cc = cc;
 			this.name = name;
-			this.type = type;
+			
 			}
 
 }
--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,8 +3,10 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota", 1800, "Altis", "Sedan");
-		System.out.println(c1.brand + "," + c1.cc + "," + c1.name + "," + c1.type);
+		Car c1 = new Car("Toyota", "Altis", 1800);
+		System.out.println(c1.brand + "," + c1.cc + "," + c1.name );
+		Car c2 = new Car("HONDA", "ACCORD", 2000);
+		Car c3 = new Car("TOYOTA","ZEE",2000);
 	}
 
 }
