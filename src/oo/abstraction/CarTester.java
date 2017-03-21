c8e4d5fde21f67c6a380ff05eea504d611caad8e
sleepcat <sleepcat10235269@gmail.com>
2017-03-21 16:21:06 +0800

    java20170321

--- src/oo/abstraction/CarTester.java
+++ src/oo/abstraction/CarTester.java
@@ -3,10 +3,29 @@ package oo.abstraction;
 public class CarTester {
 
 	public static void main(String[] args) {
-		Car c1 = new Car("Toyota", "Altis", 1800);
-		System.out.println(c1.brand + "," + c1.cc + "," + c1.name );
+		Car c1 = new Car("TOYOTA", "Altis", 1800);
 		Car c2 = new Car("HONDA", "ACCORD", 2000);
-		Car c3 = new Car("TOYOTA","ZEE",2000);
+		Car c3 = new Car("TOYOTA","WISH",2000);
+		
+		int[] n = new int[5];
+		System.out.println(n[2]);
+		
+		Car[] cars = new Car[3];
+		cars[0] = new Car("TOYOTA", "Altis", 1800);
+		cars[1] = new Car("HONDA", "ACCORD", 2000);
+		cars[2] = new Car("TOYOTA","WISH",2000);
+		cars[0].id = 300;
+		cars[1].id = 403;
+		cars[2].id = 400;
+		System.out.println(cars[0].name);
+		for(int i=0;i<3;i++){
+			System.out.println(cars[i].name);
+		}
+		
+		class AA{
+			
+		}
+		AA a = new AA();
 	}
 
 }
