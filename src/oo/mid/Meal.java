49c2608abaf5a394be8e986e00792302a7974d4e
yohanei <sleepcat10235269@gmail.com>
2017-04-25 14:24:17 +0800

    20170425java

--- src/oo/mid/Meal.java
+++ src/oo/mid/Meal.java
@@ -1,5 +1,28 @@
 package oo.mid;
 
 public class Meal {
+	String meal;
+	String attached;
+	String drink;
+	int mp;
+	int ap;
+	int dp;
+	int sum = mp + ap + dp;
+	public Meal (String meal, int mp, String attached, int ap, String drink,int dp){
+		this.meal = meal;
+		this.mp = mp;
+		this.attached = attached;
+		this.ap = ap;
+		this.drink = drink;
+		this.dp = dp;
+	}
+	
+	public void setSum(int s){
+		sum = s;
+	}
+	
+	public int addSum(int s){
+		return sum = sum + s;
+	}
 
 }
--- src/oo/mid/MealTester.java
+++ src/oo/mid/MealTester.java
@@ -3,7 +3,8 @@ package oo.mid;
 public class MealTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Meal m1 = new Meal("big mac", 39 ,"fried chicken", 25 ,"cola", 15);
+		int sum;
 
 	}
 
