8ba260582d2a4de96288f9de112f825153919b01
yohanei <sleepcat10235269@gmail.com>
2017-04-25 16:30:54 +0800

    201704250430

--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -1,5 +1,15 @@
 package oo.stati;
 
 public class Tester {
+	
+	public static void main(String[]args){
+		System.out.println(Student.pass);
+		Student stu1 = new Student(79,84,53);
+		Student stu2 = new Student(58,66,29);
+		stu1.pass = 70;
+		stu1.print();
+		stu2.print();
+//		GraduateStudent gstu1 = new GraduateStudent();
+	}
 
 }
