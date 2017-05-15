4927ddd1faad9f6409637e9f6bf7c813e5261baa
yohanei <sleepcat10235269@gmail.com>
2017-05-16 01:59:19 +0900

    Update Tester.java

--- src/oo/shopping/Tester.java
+++ src/oo/shopping/Tester.java
@@ -6,20 +6,20 @@ public class Tester {
 
 	public static void main(String[] args) {
 		
-		ArrayList<Customer>list = new ArrayList<>();
-		list.add(new Customer(6000));
-		list.add(new SilverCustomer(8000));
-		list.add(new SilverCustomer(10000));
-		list.add(new GoldenCustomer(30000));
-		list.add(new Customer(2000));
-		list.add(new GoldenCustomer(10000));
-		
-		
-		
-		for(int i = 0 ; i <= list.size() ; i++){
-			Customer cus = list.get(i);
-			cus.print();
-		}
+		Customer c1 = new Customer();
+		c1.Customer(6000);
+		SilverCustomer s1 = new SilverCustomer();
+		s1.SilverCustomer(8000);
+		SilverCustomer s2 = new SilverCustomer();
+		s2.SilverCustomer(10000);
+		GoldenCustomer g1 = new GoldenCustomer();
+		g1.SilverCustomer(30000);
+		g1.Back(30000);
+		Customer c2 = new Customer();
+		c2.Customer(2000);
+		GoldenCustomer g2 = new GoldenCustomer();
+		g2.SilverCustomer(10000);
+		g2.Back(10000);
 
 	}
 
