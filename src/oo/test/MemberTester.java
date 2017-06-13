adf6e701dc5307cde20e1e9d0dcbfb706bdec307
yohanei <sleepcat10235269@gmail.com>
2017-06-13 15:35:10 +0800

    Hiding the Implementation

---
+++ src/oo/test/MemberTester.java
@@ -0,0 +1,16 @@
+package oo.test;
+
+public class MemberTester {
+
+	public static void main(String[] args) {
+
+		Member member = new Member();
+		member.setAge(80);
+		System.out.println(member.getAge());
+		
+		//Phone Member
+		PhoneMember member2 = new PhoneMember();
+
+	}
+
+}
